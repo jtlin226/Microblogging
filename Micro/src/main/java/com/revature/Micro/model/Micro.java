e5c24e05659b5b6dde1d70d37d5c91bcd1875ef8
Jintao Lin <jtlin226@gmail.com>
2021-10-12 15:41:43 -0500

    micro model

---
+++ Micro/src/main/java/com/revature/Micro/model/Micro.java
@@ -0,0 +1,41 @@
+package com.revature.Micro.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+/**
+ * Review object model that contains user rating and
+ * micro content of a particular Ghibli Studio film
+ * and the user that created this micro
+ */
+@Entity
+@Table(name = "micros")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Micro {
+    /**
+     * Unique primary key with auto increment value for micros table
+     */
+    @Id
+    @Column(name = "review_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int microId;
+
+    /**
+     * The content of the micro post
+     */
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
+
+//    /**
+//     * The user that created this micro, foreign key referencing the User object
+//     */
+//    @ManyToOne(targetEntity = User.class)
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private User user;
+
+}
