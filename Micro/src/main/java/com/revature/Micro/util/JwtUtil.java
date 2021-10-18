ed5caf83f225fb8429346f3942cc76fc5dff1d79
DDRIV <ddriv4@gmail.com>
2021-10-18 15:46:23 -0500

    Added matcher so the recover in user could be accessed

--- Micro/src/main/java/com/revature/Micro/util/JwtUtil.java
+++ Micro/src/main/java/com/revature/Micro/util/JwtUtil.java
@@ -56,7 +56,7 @@ public class JwtUtil {
 
     private String createToken(Map<String, Object> claims, String subject) {
         Date now = new Date(System.currentTimeMillis());
-        Date expire = new Date(System.currentTimeMillis() + 1000*60*60*3); // one hour from issue time
+        Date expire = new Date(System.currentTimeMillis() + 3600000); // one hour from issue time
         return Jwts.builder()
                 .setClaims(claims) // is empty now - can add stuff to it
                 .setSubject(subject) // the username
